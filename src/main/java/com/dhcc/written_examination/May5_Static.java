58544c6822a4aac8a7f3e6be9e986c5529df5c03
lijinzhuo <872125601@qq.com>
2019-05-05 22:30:01 +0800

    新增静态代码笔试题

---
+++ src/main/java/com/dhcc/written_examination/May5_Static.java
@@ -0,0 +1,20 @@
+package com.dhcc.written_examination;
+
+/**
+ * 最后面的分号没有影响，编译不会报错
+ * 类加载会先进性静态成员初始化，静态代码块也是静态成员
+ * 初始化的顺序会按照定义的先后顺序进行初始化
+ * 10先除以3等于3在加上5等于8
+ */
+public class May5_Static {
+    static int x = 10;
+    static {
+        x /= 3;
+    }
+    public static void main(String[] args) {
+        System.out.println("x=" + x);
+    }
+    static {
+        x += 5;
+    };
+}

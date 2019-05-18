7bf9f90909a6491cbed87ce228e45ad0d23f5505
李锦卓 <872125601@qq.com>
2019-05-19 00:03:17 +0800

    May18_for_static

---
+++ src/main/java/com/dhcc/written_examination/May18_for_static.java
@@ -0,0 +1,23 @@
+package com.dhcc.written_examination;
+
+/**
+ *i<0
+ * 第一次循环 输出abdc  i++  等于1
+ * 第二次循环 输出bdc i++ 等于2
+ * 第三次循环 输出b
+ */
+public class May18_for_static {
+    static boolean out(char c) {
+        System.out.println(c);
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int i = 0;
+        for (out('A'); out('B') && (i < 2); out('C')) {
+            i++;
+            out('D');
+        }
+    }
+
+}

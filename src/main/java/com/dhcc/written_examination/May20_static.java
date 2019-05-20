ae161eb744de608b12b48cdc474433550e5d9a1d
李锦卓 <872125601@qq.com>
2019-05-21 00:09:12 +0800

    May20_static

---
+++ src/main/java/com/dhcc/written_examination/May20_static.java
@@ -0,0 +1,29 @@
+package com.dhcc.written_examination;
+
+/**
+ *输出c
+ * 如果去掉final 输出abc
+ * 因为final是一个常量 jvm直接将一个空间赋值为c
+ * 调用的时候直接输出c
+ *
+ */
+public class May20_static {
+
+    public static void main(String[] args) {
+        System.out.println(BX.c);
+    }
+
+
+}
+class AX {
+    static {
+        System.out.println("A");
+    }
+}
+class BX extends AX {
+    static {
+        System.out.println("B");
+    }
+
+    public final static String c = "C";
+}

9b0034e4344401397494ddbe843a56a7ce3dcf02
李锦卓 <872125601@qq.com>
2019-06-24 01:19:14 +0800

    June24_for

---
+++ src/main/java/com/dhcc/written_examination/June/June24_for.java
@@ -0,0 +1,14 @@
+package com.dhcc.written_examination.June;
+
+/**
+ * int的最大值是2的31次方减一 超过会成为负数
+ * 根据for循环规则 i的最后一值是2的30次方 i等于31
+ */
+public class June24_for {
+    public static void main(String[] args) {
+        for (int i = 1, j = 1; i >= 0; i = i + i, j++) {
+            System.out.println(i + "," + j);
+        }
+    }
+
+}
